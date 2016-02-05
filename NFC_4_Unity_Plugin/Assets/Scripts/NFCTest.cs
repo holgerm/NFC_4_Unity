386d279c96c6200a6e1f7a27e1f238b427438887
holgerm <mail@holgermuegge.de>
2016-02-05 10:05:05 +0100

    unity part (initial)

---
+++ NFC_4_Unity_Plugin/Assets/Scripts/NFCTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NFCTest : MonoBehaviour
+{
+	public Text tag_output_text;
+	AndroidJavaClass pluginTutorialActivityJavaClass;
+	
+	void Start ()
+	{
+#if UNITY_ANDROID
+		AndroidJNI.AttachCurrentThread ();
+		pluginTutorialActivityJavaClass = new AndroidJavaClass ("com.questmill.nfc.NFCPlugin");
+#endif
+	}
+
+
+	void Update ()
+	{
+#if UNITY_ANDROID
+		if (pluginTutorialActivityJavaClass != null) {
+			string value = pluginTutorialActivityJavaClass.CallStatic<string> ("getValue");
+			tag_output_text.text = "Value:\n" + value;
+		}
+#endif
+	}
+}
